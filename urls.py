131e682789ccc1c6516b3d75b914997b0c548fba
Ale Almuna <malmuna@csrg.cl>
2012-07-10 00:06:23 -0400

    Add README
    Add README and remove admin from urls.py

--- urls.py
+++ urls.py
@@ -15,6 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
     (r'^search/', include('haystack.urls')),
 )
